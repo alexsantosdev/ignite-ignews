542f320ef6d79bf2d1eeea7e408d00b15a4bac8c
Alex Santos <73599334+alexsantosdev@users.noreply.github.com>
2022-08-22 16:02:40 -0300

    [Redirecionando para o Stripe] Update

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+
+import { stripe } from '../../services/stripe'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if(req.method === 'POST') {
+        const session = await req.body.user
+
+        console.log(session)
+
+        const stripeCustomer = await stripe.customers.create({
+            email: session.user.email,
+            // metadata
+        })
+
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: stripeCustomer.id,
+            payment_method_types: ['card'],
+            billing_address_collection: 'required',
+            line_items: [
+                { price: 'price_1LZHAeAmnNZwkVXN5SHUbXoh', quantity: 1 },
+            ],
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL
+        })
+
+        return res.status(200).json({ sessionId: stripeCheckoutSession.id})
+    }else {
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed')
+    }
+}
